cb3152b94909ec1284793c843e48f1735985a86e
Danil Doroshin <ddddanil5555@gmail.com>
2021-12-18 16:44:09 +0300

    Rewrite parser row via subsume and add state interpreter

--- lib/Polysemy/Megaparsec.hs
+++ lib/Polysemy/Megaparsec.hs
@@ -2,10 +2,13 @@ module Polysemy.Megaparsec (
   Parsec
 , lift
 , runParser, runParser', runParserMaybe
+, runParserState
 ) where
 
 import Prelude
 import Polysemy
+import Polysemy.State
+import Polysemy.Internal
 import qualified Control.Monad.Trans.Class as T
 import qualified Text.Megaparsec as MP
 import Data.Either.Combinators
@@ -15,12 +18,19 @@ type Parsec e s r a = MP.ParsecT e s (Sem r) a
 lift :: Sem r a -> Parsec e s r a
 lift = T.lift
 
-runParser :: Parsec e s r a -> String -> s -> Sem r (Either (MP.ParseErrorBundle s e) a)
-runParser = MP.runParserT
+runParser :: (Subsume rP rM) => Parsec e s rP a -> String -> s -> Sem rM (Either (MP.ParseErrorBundle s e) a)
+runParser p n s = subsume_ $ MP.runParserT p n s
 
-runParser' :: Parsec e s r a -> MP.State s e -> Sem r (MP.State s e, Either (MP.ParseErrorBundle s e) a)
-runParser' = MP.runParserT'
+runParser' :: (Subsume rP rM) => Parsec e s rP a -> MP.State s e -> Sem rM (MP.State s e, Either (MP.ParseErrorBundle s e) a)
+runParser' p s = subsume_ $ MP.runParserT' p s
 
-runParserMaybe :: (Ord e, MP.Stream s) => Parsec e s r a -> s -> Sem r (Maybe a)
+runParserMaybe :: (Subsume rP rM, Ord e, MP.Stream s) => Parsec e s rP a -> s -> Sem rM (Maybe a)
 runParserMaybe p s = rightToMaybe <$> runParser (p <* MP.eof) "" s
+
+runParserState :: Parsec e s r a -> Sem (State (MP.State s e) ': r) (Either (MP.ParseErrorBundle s e) a)
+runParserState p = do
+  s <- get
+  (s', a) <- runParser' p s
+  put s'
+  return a
 
